4030717257cde73ea26ba05939d37b743b2c2deb
Xiaokeai18 <im.wangpei@outlook.com>
2019-02-14 16:56:34 +0800

    update

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -43,7 +43,7 @@ for i=1:length(lambda_vec)
     theta = trainLinearReg(X, y, lambda);
     error_train(i) = linearRegCostFunction(X, y, theta, 0);
     error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
-end;
+end
 
 
 % =========================================================================
